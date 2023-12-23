70f3bbf28e6dedfa9ff8e5cae9c2dc4403d8d6d7
backas36 <backas36@gmail.com>
2023-12-24 00:39:06 +0800

    test: Add missing tests for Counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 
 interface IProps {
@@ -6,10 +7,34 @@ interface IProps {
 }
 const Counter = ({ defaultCount, desc }: IProps) => {
   const [count, setCount] = useState(defaultCount);
+  const [incrementor, setIncrementor] = useState(1);
   return (
     <div>
       <h2>{desc}</h2>
+      <label htmlFor="incrementor">
+        Incrementor:
+        <input
+          type="text"
+          id="incrementor"
+          className="border-2"
+          value={incrementor}
+          onChange={(e) => setIncrementor(parseInt(e.target.value) || 0)}
+        />
+      </label>
+      <br />
+      <button
+        aria-label="Increment Counter"
+        onClick={() => setCount((prev) => prev + incrementor)}
+      >
+        +
+      </button>
       <div className="">Current Count: {count}</div>
+      <button
+        aria-label="Decrement Counter"
+        onClick={() => setCount((prev) => prev - incrementor)}
+      >
+        -
+      </button>
     </div>
   );
 };
--- src/components/Counter/Couter.test.tsx
+++ src/components/Counter/Couter.test.tsx
@@ -1,11 +1,88 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Counter from "./Counter";
 
-it("defaultCount=0, then counter =1", () => {
-  render(<Counter defaultCount={0} desc="My Counter" />);
-  expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
-  expect(screen.getByText(/my counter/i)).toBeInTheDocument();
+const user = userEvent.setup();
+
+describe("Counter", () => {
+  describe("initialized with defaultCount=10, desc='My Counter'", () => {
+    beforeEach(() => {
+      render(<Counter defaultCount={10} desc="My Counter" />);
+    });
+
+    it("render 'Current Count: 10'", () => {
+      expect(screen.getByText("Current Count: 10")).toBeInTheDocument();
+    });
+
+    it("renders title as 'My Counter'", () => {
+      expect(screen.getByText(/my counter/i)).toBeInTheDocument();
+    });
+
+    describe("when + is clicked", () => {
+      it("renders 'Current Counter:11", async () => {
+        await user.click(
+          screen.getByRole("button", { name: "Increment Counter" }),
+        );
+
+        expect(screen.getByText("Current Count: 11")).toBeInTheDocument();
+      });
+    });
+
+    describe("when - is clicked", () => {
+      it("renders 'Current Counter: 9", async () => {
+        await user.click(
+          screen.getByRole("button", { name: "Decrement Counter" }),
+        );
+
+        expect(screen.getByText("Current Count: 9")).toBeInTheDocument();
+      });
+    });
+
+    describe("when the incrementor changes to 5 and '+' button is clicked", () => {
+      it("renders 'Current Count: 15", async () => {
+        const input = screen.getByLabelText(/incrementor:/i);
+        await user.clear(input);
+        await user.type(input, "5");
+        await user.click(
+          screen.getByRole("button", { name: "Increment Counter" }),
+        );
+        expect(screen.getByText("Current Count: 15")).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("initialized with defaultCount=0, desc='My Counter'", () => {
+    beforeEach(() => {
+      render(<Counter defaultCount={0} desc="My Counter" />);
+    });
+
+    it("render 'Current Count: 0'", () => {
+      expect(screen.getByText("Current Count: 0")).toBeInTheDocument();
+    });
+
+    it("renders title as 'My Counter'", () => {
+      expect(screen.getByText(/my counter/i)).toBeInTheDocument();
+    });
+
+    describe("when + is clicked", () => {
+      it("renders 'Current Counter:1", async () => {
+        await user.click(
+          screen.getByRole("button", { name: "Increment Counter" }),
+        );
+
+        expect(screen.getByText("Current Count: 1")).toBeInTheDocument();
+      });
+    });
+
+    describe("when - is clicked", () => {
+      it("renders 'Current Counter: -1", async () => {
+        await user.click(
+          screen.getByRole("button", { name: "Decrement Counter" }),
+        );
+
+        expect(screen.getByText("Current Count: -1")).toBeInTheDocument();
+      });
+    });
+  });
 });
-it.todo("defaultCount=0, + clicked then counter = 1", () => {});
-it.todo("defaultCount=0, - clicked then counter = -1", () => {});
